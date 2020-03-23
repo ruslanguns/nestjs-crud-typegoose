6999e6abe18abeda0770dd15f4a01ad0a7cecde2
Ruslan Gonzalez <ruslanguns@gmail.com>
2020-03-23 18:48:13 +0000

    added mongoose module

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+@Module({
+    imports: [
+        MongooseModule.forRoot('mongodb://localhost:27017/nestjs-typegoose', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }),
+    ],
+})
+export class AppModule { }
